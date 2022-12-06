7190dcfa2659db8802ac9af4c988af1efa30e8ea
pathymavuba <mavubapathy@gmail.com>
2022-12-06 16:01:29 +0100

    day1

---
+++ dist/day1/index.js
@@ -0,0 +1,26 @@
+"use strict";
+// const {readFileSync, promises: fsPromises} = require('fs');
+// function syncReadFile(filename:any) {
+//     const contents = readFileSync(filename, 'utf-8');
+//     const arr = contents.split('\n');
+//     console.log(arr); // 👉️ ['One', 'Two', 'Three', 'Four']
+//     return arr;
+//   }
+//   syncReadFile('./src/day1/input.txt');
+const fs = require('fs');
+const inputData = fs.readFileSync('./src/day1/input.txt', 'utf8');
+const inputArray = inputData.split('\n');
+let mostCalories = 0;
+let curentCount = 0;
+inputArray.forEach((line) => {
+    if (line == '') {
+        if (curentCount > mostCalories) {
+            mostCalories = curentCount;
+        }
+        curentCount = 0;
+    }
+    else {
+        curentCount += Number(line);
+    }
+});
+console.log(mostCalories);
---
+++ src/day1/index.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+var inputData = fs.readFileSync('input.txt', 'utf-8');
+console.log(inputData);
