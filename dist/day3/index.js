858fb949677914da9fe7662ba66f21ed827211e9
pathymavuba <mavubapathy@gmail.com>
2022-12-06 17:53:23 +0100

    day3 part1

---
+++ dist/day3/index.js
@@ -0,0 +1,21 @@
+"use strict";
+const fs = require('fs');
+const inputData = fs.readFileSync('./src/day3/input.txt', 'utf8');
+const inputArray = inputData.split('\n');
+const priorities = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const chars = [];
+let score = 0;
+inputArray.forEach((line) => {
+    const first = line.slice(0, line.length / 2);
+    const second = line.slice(line.length / 2, line.length);
+    for (const c of first) {
+        if (second.includes(c)) {
+            chars.push(c);
+            break;
+        }
+    }
+});
+chars.forEach((c) => {
+    score += priorities.indexOf(c) + 1;
+});
+console.log(score);
---
+++ src/day3/index.ts
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const inputData = fs.readFileSync('./src/day3/input.txt', 'utf8');
+
+
+const inputArray = inputData.split('\n');
+
+const priorities = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const chars: string[] = [];
+let score = 0;
+
+inputArray.forEach((line:any) => {
+	const first = line.slice(0, line.length / 2);
+	const second = line.slice(line.length / 2, line.length);
+
+	for (const c of first) {
+		if (second.includes(c)) {
+			chars.push(c);
+			break;
+		}
+	}
+});
+
+chars.forEach((c) => {
+	score += priorities.indexOf(c) + 1;
+});
+
+console.log(score);
